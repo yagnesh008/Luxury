ac3934a0a9da3f74c453b4f3d01ef7ec6dee53e2
yagnesh <yagnesh007d@gmail.com>
2026-04-26 21:21:37 +0530

    db update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,10 @@
 const { Pool } = require("pg");
 
+if (!process.env.DATABASE_URL) {
+  console.error("❌ DATABASE_URL is missing!");
+  process.exit(1); // 💥 stop app immediately
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
