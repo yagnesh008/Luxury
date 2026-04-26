90d844d66f2f60f02a470efd2182e7bf12ba75c8
yagnesh <yagnesh007d@gmail.com>
2026-04-26 20:53:50 +0530

    connect supabase db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
