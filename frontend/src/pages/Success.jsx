02587f7ac848134de6d08377fd1c282a6302fe63
yagnesh <yagnesh007d@gmail.com>
2026-04-27 12:44:56 +0530

    fix routing

--- frontend/src/pages/Success.jsx
+++ frontend/src/pages/Success.jsx
@@ -1,9 +1,39 @@
-import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useCart } from "../context/CartContext";
+import API from "../services/api";
 
 
 export default function Success() {
   const navigate = useNavigate();
+    const { setCart } = useCart(); 
+  useEffect(() => {
+    const clearAfterPayment = async () => {
+      const userId = localStorage.getItem("userId");
+
+      console.log("CLEARING CART FOR:", userId); 
+
+      if (!userId) return;
+
+      try {
+        // 🔥 1. Clear backend
+        await API.delete(`/cart/clear/${userId}`);
+
+        // 🔥 2. Clear frontend state
+        setCart([]);
+
+        // 🔥 3. Clear localStorage
+        localStorage.removeItem("cart");
+
+        console.log("CART CLEARED ✅");
+
+      } catch (err) {
+        console.error("CLEAR CART ERROR ❌", err);
+      }
+    };
+
+    clearAfterPayment();
+  }, []);
   return (
     <div className="success-page">
       <div className="success-box">
