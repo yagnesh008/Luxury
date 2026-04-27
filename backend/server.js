d4a52dd23f39a9ab4ed08b0422b75cac6ab5bb20
yagnesh <yagnesh007d@gmail.com>
2026-04-27 14:18:32 +0530

    fix routing

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const pool = require("./config/db");
+const cartRoutes = require("./routes/cart");
 
 const app = express();
 
